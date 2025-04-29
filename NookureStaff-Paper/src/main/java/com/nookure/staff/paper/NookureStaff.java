09e2e4af0a1d1dc2cd96be4feab43768aea0f620
Lunna5 <git@lunna.dev>
2025-04-29 21:40:00 +0200

    feat(player): add listener for player advancement criterion grant events

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/NookureStaff.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/NookureStaff.java
@@ -22,6 +22,7 @@ import com.nookure.staff.api.extension.staff.StaffModeExtension;
 import com.nookure.staff.api.messaging.Channels;
 import com.nookure.staff.api.messaging.EventMessenger;
 import com.nookure.staff.api.util.AbstractLoader;
+import com.nookure.staff.api.util.ServerUtils;
 import com.nookure.staff.paper.bootstrap.StaffBootstrapper;
 import com.nookure.staff.paper.command.*;
 import com.nookure.staff.paper.command.main.NookureStaffCommand;
@@ -183,8 +184,12 @@ public class NookureStaff {
           OnPlayerAttack.class,
           OnWorldChange.class,
           OnStaffPlayerCommand.class,
-          OnSpawnerSpawn.class
+          OnPlayerAdvancementCriterionGrant.class
       ).forEach(this::registerListener);
+
+      if (ServerUtils.MINECRAFT_VERSION.isAtLeast(1, 21, 0)) {
+        registerListener(OnSpawnerSpawn.class);
+      }
     }
 
     if (config.get().staffMode.silentChestOpen()) {
---
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/listener/staff/state/OnPlayerAdvancementCriterionGrant.java
@@ -0,0 +1,24 @@
+package com.nookure.staff.paper.listener.staff.state;
+
+import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
+import com.google.inject.Inject;
+import com.nookure.staff.api.manager.PlayerWrapperManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class OnPlayerAdvancementCriterionGrant implements Listener {
+  @Inject
+  private PlayerWrapperManager<Player> playerWrapperManager;
+
+  @EventHandler
+  public void onPlayerAdvancementDone(PlayerAdvancementCriterionGrantEvent event) {
+    final var staffPlayer = playerWrapperManager.getStaffPlayerOrNull(event.getPlayer().getUniqueId());
+
+    if (staffPlayer == null) {
+      return;
+    }
+
+    event.setCancelled(staffPlayer.isStaffModeOrVanish());
+  }
+}
