56b97bc77b6bbb6d746711f50efdbd19aaf47eb9
Lunna5 <git@lunna.dev>
2025-05-10 18:48:33 +0200

    fix(event): register strong references by default to avoid the GC when needed, fixes #160

--- NookureStaff-API/src/main/java/com/nookure/staff/api/event/EventVector.java
+++ NookureStaff-API/src/main/java/com/nookure/staff/api/event/EventVector.java
@@ -1,8 +1,39 @@
 package com.nookure.staff.api.event;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.lang.reflect.Method;
 
-public record EventVector(Method method, Reference<Object> listener, NookSubscribe nookSubscribe) {
+public record EventVector(Method method, Object listener, NookSubscribe nookSubscribe, boolean weak) {
+  /**
+   * Create a new event vector
+   *
+   * @param method        The method to call
+   * @param listener      The listener to call the method on
+   * @param nookSubscribe The nook subscribe annotation
+   * @param weak          Whether the listener is weak or not
+   */
+  public EventVector(Method method, Object listener, NookSubscribe nookSubscribe, boolean weak) {
+    this.method = method;
+    this.listener = weak ? new WeakReference<>(listener) : listener;
+    this.nookSubscribe = nookSubscribe;
+    this.weak = weak;
+  }
 
+  /**
+   * Get the listener
+   *
+   * @return The listener
+   */
+  @Nullable
+  public Object listener() {
+    if (weak) {
+      Reference<?> reference = (Reference<?>) listener;
+      return reference.get();
+    } else {
+      return listener;
+    }
+  }
 }
