8e7d4b0c7f189d858d69be53b9b1f38e5e434803
Lunna5 <git@lunna.dev>
2026-05-23 18:36:36 +0200

    refactor: update TeleportToPlayerAction to use constructor injection and improve logging

--- NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/action/TeleportToPlayerAction.java
+++ NookureStaff-Paper/src/main/java/com/nookure/staff/paper/inventory/action/TeleportToPlayerAction.java
@@ -9,18 +9,27 @@ import com.nookure.staff.api.config.bukkit.BukkitMessages;
 import com.nookure.staff.api.manager.PlayerWrapperManager;
 import com.nookure.core.inv.paper.annotation.CustomActionData;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.UUID;
 
 @CustomActionData(value = "TELEPORT_TO_PLAYER", hasValue = true)
 public class TeleportToPlayerAction extends CustomPaperAction {
+  private final PlayerWrapperManager<Player> playerWrapperManager;
+  private final ConfigurationContainer<BukkitMessages> messages;
+  private final Logger logger;
+
   @Inject
-  private PlayerWrapperManager<Player> playerWrapperManager;
-  @Inject
-  private ConfigurationContainer<BukkitMessages> messages;
-  @Inject
-  private Logger logger;
+  public TeleportToPlayerAction(
+      @NotNull final PlayerWrapperManager<Player> playerWrapperManager,
+      @NotNull final ConfigurationContainer<BukkitMessages> messages,
+      @NotNull final Logger logger
+  ) {
+    this.playerWrapperManager = playerWrapperManager;
+    this.messages = messages;
+    this.logger = logger;
+  }
 
   @Override
   public void execute(Player player, @Nullable String value) {
@@ -35,15 +44,12 @@ public class TeleportToPlayerAction extends CustomPaperAction {
       return;
     }
 
-    // The value can be either a player name or UUID
     PlayerWrapper targetPlayer = null;
     
-    // Try to parse as UUID first
     try {
       UUID targetUUID = UUID.fromString(value);
       targetPlayer = playerWrapperManager.getPlayerWrapper(targetUUID).orElse(null);
     } catch (IllegalArgumentException e) {
-      // Not a UUID, try to find by name
       targetPlayer = playerWrapperManager.stream()
           .filter(p -> p.getName().equalsIgnoreCase(value))
           .findFirst()
@@ -55,10 +61,9 @@ public class TeleportToPlayerAction extends CustomPaperAction {
       return;
     }
 
-    // Use native teleportation API
     staffPlayer.teleport(targetPlayer);
     staffPlayer.sendMiniMessage(messages.get().staffMode.teleportingTo(), "player", targetPlayer.getName());
     
-    logger.debug("Teleported %s to %s using native API", staffPlayer.getName(), targetPlayer.getName());
+    logger.debug("[Inventory] Teleported %s to %s", staffPlayer.getName(), targetPlayer.getName());
   }
 } 
